6ccd6ece49620a4101ffeb03ff1771293afe9528
Santa claus <santa@northpole.com>
2020-05-12 00:28:50 +0300

    addition of more tests

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -5,8 +5,8 @@ import unittest
 class CommentModelTest(unittest.TestCase):
   def setUp(self):
     self.user_victor = User(username = 'kennedy',password = '0711468410', email = 'kennedymbithi12@gmail.com')
-    self.new_blog = Pitch(id=1,title='Test',content='This is a test pitch',user = self.user_victor)
-    self.new_comment = Comment(id=1, content='Test comment',user=self.user_victor,blog=self.new_blog)
+    self.new_blog = Pitch(id=1,title='Test',content='This is a test pitch',user = self.user_kennedy)
+    self.new_comment = Comment(id=1, content='Test comment',user=self.user_kennedy,blog=self.new_blog)
 
 
   def test_check_instance_variables(self):
