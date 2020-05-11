fe71dc8b4f3e157e97faf380c1a1ba16967def95
Santa claus <santa@northpole.com>
2020-05-12 00:27:36 +0300

    addition of tests

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -4,7 +4,7 @@ import unittest
 
 class CommentModelTest(unittest.TestCase):
   def setUp(self):
-    self.user_victor = User(username = 'victor',password = 'victor', email = 'victor@ms.com')
+    self.user_victor = User(username = 'kennedy',password = '0711468410', email = 'kennedymbithi12@gmail.com')
     self.new_blog = Pitch(id=1,title='Test',content='This is a test pitch',user = self.user_victor)
     self.new_comment = Comment(id=1, content='Test comment',user=self.user_victor,blog=self.new_blog)
 
