477efed780b653439be52c42c66923bc1f3ee031
Santa claus <santa@northpole.com>
2020-05-12 13:22:28 +0300

    deployment

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
   General configuration parent class
   '''
   SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kennedy:Kennedy128@localhost/bloge'
+  
   UPLOADED_PHOTOS_DEST='app/static/photos'
   #  email configurations
   MAIL_SERVER = 'smtp.gmail.com'
